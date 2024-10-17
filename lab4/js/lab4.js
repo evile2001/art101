823e0277b9e6048aaf659dc501cde3758615af51
evile2001 <faszeremma@gmail.com>
2024-10-17 10:46:46 -0700

    Create lab4.js

---
+++ lab4/js/lab4.js
@@ -0,0 +1,13 @@
+//Flappy-Bird
+//An interpretation of the original game
+//Autania Mierow and Emma Faszer
+//October 2024
+
+//Generate light blue sky with white clouds as background
+//Generate pipes of different lengths that come out from the top and bottom of the screen
+    //Leave gaps between the top and bottom pipes
+    //Alternate the lengths of the pipes as you move through the map
+//Generate a little yellow bird, movement controlled by player’s space bar
+    //Objective is for bird to pass through the pipes without hitting them
+//When the bird hits the pipe, add large “Game Over” graphic
+//Regenerate and go again
