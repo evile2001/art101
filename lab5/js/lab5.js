f27a2a3e228770951f359d5292cc96892afa88ee
evile2001 <faszeremma@gmail.com>
2024-10-21 17:45:22 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Emma Faszer <efaszer@ucsc.edu>
+ * Created: 21 October 2024
+ * Licence: Public Domain
+ * /
+
+// Define variables
+make = "Toyota" ;
+model = "Highlander Hybrid" ;
+color = "blue" ;
+year = "2008" ;
+
+// calculate
+age = 2024 - 2008;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln(Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
