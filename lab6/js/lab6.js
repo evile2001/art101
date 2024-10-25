0dcca25c3dcec048966de03f65102f2cb8777671
evile2001 <faszeremma@gmail.com>
2024-10-24 18:09:50 -0700

    Create lab6.js

---
+++ lab6/js/lab6.js
@@ -0,0 +1,21 @@
+// Author: Emma Faszer <efaszer@ucsc.edu>
+// Created: 24 October 2024
+
+// Define variables
+myTransport = ["Car", "Walk", "Bike", "Bus"]
+
+// Object
+myMainRide = {
+    make: "Toyota",
+    model: "Highlander",
+    color: "blue",
+    year: "2008",
+    age: function() {
+        return 2024 - this.year;
+    }
+}
+
+// Output
+document.writeln("How I get around: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
