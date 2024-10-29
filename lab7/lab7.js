f7d05704d9b3abd733bbe6d05e1bea85c3303e32
evile2001 <faszeremma@gmail.com>
2024-10-29 00:13:26 -0700

    Create lab7.js

---
+++ lab7/lab7.js
@@ -0,0 +1,30 @@
+/*
+* Author: Emma Faszer <efaszer@ucsc.edu>
+* Created: 28 October 2024
+*/
+
+function sortUserName () {
+var userName = window.prompt ("Hi, please tell me your name so I can fix it.");
+console.log ("userName =", userName);
+
+// turn string into array
+var nameArray = userName.split ('');
+console.log ("nameArray =", nameArray);
+
+// sort array
+var nameArraySort = nameArray.sort ();
+console.log ("nameArraySort =", nameArraySort);
+
+// array back to string
+var nameSorted = nameArraySort.join ('');
+console.log ("nameSorted =", nameSorted);
+
+return "Your sorted name is: " + nameSorted;
+}
+
+function displaySortedName () {
+let result = sortUserName ();
+document.getElementBId("output").innerHTML = '<p>I fixed your name: <strong>${result}</strong></p>';
+}
+
+window.onload = displaySortedName;
