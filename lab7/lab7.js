7df34cbce04bf5972fe061632648f15337c9a0ac
evile2001 <faszeremma@gmail.com>
2024-10-28 20:14:12 -0700

    Update lab7.js

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -7,16 +7,12 @@
 function sortUserName() {
     var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
     console.log("userName =", userName);
-
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
-
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
-
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
-
     return nameSorted;
 }
 sortUserName();
