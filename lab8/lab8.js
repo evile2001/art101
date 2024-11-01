4d917e1a1a5f82379a53e62b1a373b9d99b2b58d
evile2001 <faszeremma@gmail.com>
2024-10-31 18:07:52 -0700

    Update lab8.js

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -4,18 +4,16 @@
 */
 
 let numbers = ["1", "2", "3", "4", "5"];
-
 let x = numbers;
-
 function multiplyByTwo (x) {
     return x*2;
 }
 
-numbers.map(multiplyByTwo);
+let numbers.map(multiplyByTwo);
 
-numbers.map(function(x){
+let numbers.map(function(x){
     return x ** 0.5;
 })
 
-var mapResults = numbers.map(multiplyByTwo);
+let mapResults = numbers.map(multiplyByTwo);
 console.log("Results: ", mapResults);
