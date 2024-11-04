87ae4182f8105ffbc3f5b27f995ebb02ed2d8ac5
evile2001 <faszeremma@gmail.com>
2024-11-04 01:37:54 -0800

    Update lab9.js

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -9,3 +9,27 @@ $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 $("#button-challenge").click(function(){
     $("#challenge").toggleClass("special");
 });
+
+// add button to problems section
+$("problems").append("button id='button-problems'>Make Special</button>");
+
+// add a listener to the problems button
+$("#button-problems").click(function(){
+    $("#problems").toggleClass("special");
+});
+
+// add button to reflection section
+$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+
+// add a listener to the reflection button
+$("#button-reflection").click(function(){
+    $("#reflection").toggleClass("special");
+});
+
+// add button to the results section
+$("#results").append("<button id='button-results'>Make Special</button>");
+
+// add a listener to the results section
+$("#button-results").click(function(){
+    $("#results").toggleClass("special");
+});
