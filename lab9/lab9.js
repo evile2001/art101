53b9aa10f69b7e7ba3ebdba3ae962ffb0b4da6be
evile2001 <faszeremma@gmail.com>
2024-11-04 01:19:52 -0800

    Update lab9.js

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -2,8 +2,10 @@
 // Date: 2 November 2024
 
 
+// add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
+// add a listener to the challenge button
 $("#button-challenge").click(function(){
-    $("challenge").toggleClass("special");
+    $("#challenge").toggleClass("special");
 });
