9244ca721e8c566de126db72706c22e26bfb27a3
evile2001 <faszeremma@gmail.com>
2024-11-18 20:22:04 -0800

    Create lab13.js

---
+++ lab13/lab13.js
@@ -0,0 +1,36 @@
+// Author: Emma Faszer
+// Date: 18 November 2024
+
+
+function processInput () {
+    const inputNumber = parseInt(document.GetElementById("numberInput").value);
+}
+
+if (isNaN(inputNumber) || inputNumber<= 0) {
+    alert("Enter a positive number");
+    return;
+}
+
+$("output").html("");
+
+for (let num = 1; num <= inputNumber; num++) {
+    let str = "";
+    let className = "";
+
+    if (num % 3 === 0) {
+        str += "Fizz";
+        className += "fizz";
+    }
+    if (num % 5 === 0) {
+        str += "Buzz";
+        className += "buzz";
+    }
+    if (num % 7 === 0) {
+        str += "Boom";
+        className += "boom";
+    }
+    if (str === "") {
+        str - num;
+    }
+    $("output").append(`<p class="${className.trim()}">${str}</p>`);
+}
