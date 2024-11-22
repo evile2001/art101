16ac79b3c3319f5704c2302b3307954b1a033a98
evile2001 <faszeremma@gmail.com>
2024-11-21 23:05:33 -0800

    9

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -2,34 +2,20 @@
 // Date: 2 November 2024
 
 
-// add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#challenge").append("<button id='button-challenge'>Make brat</button>");
+$("#problems").append("<button id='button-problems'>Make brat</button>");
+$("#reflection").append("<button id='button-reflection'>Make brat</button>");
+$("#results").append("<button id='button-results'>Make brat</button>");
 
-// add a listener to the challenge button
-$("#button-challenge").click(function(){
+$("#button-challenge").click(function() {
     $("#challenge").toggleClass("special");
 });
-
-// add button to problems section
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-
-// add a listener to the problems button
-$("#button-problems").click(function(){
+$("#button-problems").click(function() {
     $("#problems").toggleClass("special");
 });
-
-// add button to reflection section
-$("#reflection").append("<button id='button-reflection'>Make Special</button>");
-
-// add a listener to the reflection button
-$("#button-reflection").click(function(){
+$("#button-reflection").click(function() {
     $("#reflection").toggleClass("special");
 });
-
-// add button to the results section
-$("#results").append("<button id='button-results'>Make Special</button>");
-
-// add a listener to the results section
-$("#button-results").click(function(){
+$("#button-results").click(function() {
     $("#results").toggleClass("special");
 });
